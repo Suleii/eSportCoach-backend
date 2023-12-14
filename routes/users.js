ee4108873b87573bb0b45f7e9ef9d2047a5183ec
olivpl <oli.pala@outlook.fr>
2023-12-14 16:23:03 +0100

    back maj

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,61 @@
+// var express = require('express');
+// var router = express.Router();
+
+// require('../models/connection');
+// const User = require('../models/usersLogin');
+// const { checkBody } = require('../modules/checkBody');
+// const uid2 = require('uid2');
+// const bcrypt = require('bcrypt');
+
+// router.post('/signup', (req, res) => {
+//   if (!checkBody(req.body, ['firstname', 'username', 'password'])) {
+//     res.json({ result: false, error: 'Missing or empty fields' });
+//     return;
+//   }
+
+//   // Check if the user has not already been registered
+//   User.findOne({ username: req.body.username }).then(data => {
+//     if (data === null) {
+//       const hash = bcrypt.hashSync(req.body.password, 10);
+
+//       const newUser = new User({
+//         firstname: req.body.firstname,
+//         username: req.body.username,
+//         password: hash,
+//         isCoach: req.body.isCoach,
+//         token: uid2(32),
+//       });
+
+//       newUser.save().then(newDoc => {
+//         res.json({ result: true, token: newDoc.token });
+//       });
+//     } else {
+//       // User already exists in database
+//       res.json({ result: false, error: 'User already exists' });
+//     }
+//   });
+// });
+
+// router.post('/signin', (req, res) => {
+//   if (!checkBody(req.body, ['username', 'password'])) {
+//     res.json({ result: false, error: 'Missing or empty fields' });
+//     return;
+//   }
+
+//   User.findOne({ username: req.body.username }).then(data => {
+//     if (data && bcrypt.compareSync(req.body.password, data.password)) {
+//       res.json({ result: true, firstname: data.firstname, token: data.token });
+//     } else {
+//       res.json({ result: false, error: 'User not found or wrong password' });
+//     }
+//   });
+// });
+
+
+
+// module.exports = router;
+
+
 var express = require('express');
 var router = express.Router();
 
@@ -8,7 +66,7 @@ const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
 router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['firstname', 'username', 'password'])) {
+  if (!checkBody(req.body, ['lastname', 'firstname', 'email', 'username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
@@ -19,7 +77,9 @@ router.post('/signup', (req, res) => {
       const hash = bcrypt.hashSync(req.body.password, 10);
 
       const newUser = new User({
+        lastname: req.body.lastname,
         firstname: req.body.firstname,
+        email: req.body.email,
         username: req.body.username,
         password: hash,
         isCoach: req.body.isCoach,
@@ -27,8 +87,11 @@ router.post('/signup', (req, res) => {
       });
 
       newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc.token });
+        console.log(newDoc)
+        res.json({ result: true, lastname: newDoc.lastname, firstname: newDoc.firstname, email: newDoc.email, username: newDoc.username ,token: newDoc.token });
       });
+      
+    
     } else {
       // User already exists in database
       res.json({ result: false, error: 'User already exists' });
@@ -54,3 +117,6 @@ router.post('/signin', (req, res) => {
 
 
 module.exports = router;
+
+
+
