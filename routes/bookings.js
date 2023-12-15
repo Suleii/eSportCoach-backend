e655b885e7575a856962c188116753c6dcae67d2
Elise <elise.jenequin@gmail.com>
2023-12-16 00:56:49 +0100

    event

--- routes/bookings.js
+++ routes/bookings.js
@@ -6,6 +6,7 @@ const UserLogin = require('../models/usersLogin')
 const CoachProfile = require('../models/coachesProfile')
 const UserProfile = require('../models/usersProfile')
 
+//Create a new booking 
 router.post('/', (req, res) => {
   const newBooking = new Booking ({
     date: req.body.date,
@@ -19,21 +20,21 @@ router.post('/', (req, res) => {
   })
 });
 
+// Delete a booking using the coach username, gamer username, date and name of game
 router.delete('/', (req, res) => {
-Booking.deleteOne( 
-  { date: req.body.date,
-    coachUsername: req.body.coachUsername,
-    game: req.body.game,
-    username: req.body.username,})
-  .then(data => {
-    if (!data) {
-      res.json({result: false, message: 'no booking found'})
-    } else {
-      res.json({result: true, message:'booking succesfully deleted'})
-    }
-  })  
-})
+  Booking.deleteOne( 
+            { _id:req.body._id,})
+            .then(data => {
+              if (!data) {
+                res.json({result: false, message: 'no booking found'})
+              } else {
+                res.json({result: true, message:'booking succesfully deleted'})
+              }
+            })  
+          })
+      
 
+// Get all the bookings of a coach with his token 
 router.get('/:token', (req, res) => {
   UserLogin.findOne({token: req.params.token}) 
   .then(user => {
