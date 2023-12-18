d9fe3b02af53b153e3f470df770771f5ca068d80
Yasmine <Your email>
2023-12-18 09:40:27 +0100

    reviews

--- models/reviews.js
+++ models/reviews.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const reviewSchema = mongoose.Schema({
     game: String,
-    username: {type:mongoose.Schema.Types.ObjectId, ref: 'userslogins'},
-    photo: {type:mongoose.Schema.Types.ObjectId, ref: 'usersprofile'},
+    username: {type:mongoose.Schema.Types.ObjectId, ref: 'usersprofile'},
     coach: {type:mongoose.Schema.Types.ObjectId, ref: 'coachesprofile'},
     content: String,
     rating: { type: Number, min: 1, max: 5 },
