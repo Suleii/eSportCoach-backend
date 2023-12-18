bd6d48a1d77b49f1b6ad14a9c56169f85d1527c4
Your name~Suleiman <Your email[201~suleiman.mulla1504@gmail.com>
2023-12-18 17:39:34 +0100

    save

--- models/coachesProfile.js
+++ models/coachesProfile.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const coachProfileSchema = mongoose.Schema({
-	lastname: String,
-    firstname: String,
-    email: String,
-    photo: String,
-    user: {type:mongoose.Schema.Types.ObjectId, ref: 'userslogins'},
-    games: [String],
-    price: Number,
-    socials: {
-        twitch: String,
-        instagram: String,
-        youtube: String,
-        discord: String,        
-            },
-    about: String,
-    experience: [String],
-    rating: Number,
+  lastname: String,
+  firstname: String,
+  email: String,
+  photo: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "userslogins" },
+  games: [String],
+  price: Number,
+  socials: {
+    twitch: String,
+    instagram: String,
+    youtube: String,
+    discord: String,
+  },
+  about: String,
+  experience: [String],
+  rating: Number,
+  language: [String],
 });
 
-const CoachProfile = mongoose.model('coachesprofile', coachProfileSchema);
+const CoachProfile = mongoose.model("coachesprofile", coachProfileSchema);
 
 module.exports = CoachProfile;
--- routes/coaches.js
+++ routes/coaches.js
@@ -1,93 +1,116 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const CoachProfile = require('../models/coachesProfile'); 
-const UserLogin = require('../models/usersLogin');
+const CoachProfile = require("../models/coachesProfile");
+const UserLogin = require("../models/usersLogin");
 
-
-  
 // Create a GET /profile route to collect the coach informations via his username
-router.get('/profile/:coach', (req, res) => {
-    // Search the user ID via his username
-    UserLogin.findOne({username: req.params.coach}) 
-    .then(user => { 
-        // Search coach ID via his user ID 
-        CoachProfile.findOne({user:user._id}).populate('user') 
-        .then (coach =>{
-        console.log(coach)
-        res.json({result:true, profile: coach})
-        })
-    }) 
-})
+router.get("/profile/:coach", (req, res) => {
+  // Search the user ID via his username
+  UserLogin.findOne({ username: req.params.coach }).then((user) => {
+    // Search coach ID via his user ID
+    CoachProfile.findOne({ user: user._id })
+      .populate("user")
+      .then((coach) => {
+        console.log(coach);
+        res.json({ result: true, profile: coach });
+      });
+  });
+});
 
 // Create a PUT /profile route to update the coach informations
-router.put('/profile/:coach', (req, res) => {
-    // Search the user ID via his username
-    UserLogin.findOne({ username: req.params.coach })
-        .then(user => {
-            if (!user) {
-                res.json({ result: false, message: 'User not found' });
-                return;
-            }
+router.put("/profile/:coach", (req, res) => {
+  // Search the user ID via his username
+  UserLogin.findOne({ username: req.params.coach }).then((user) => {
+    if (!user) {
+      res.json({ result: false, message: "User not found" });
+      return;
+    }
 
-            // Update the coach profile by user ID
-            CoachProfile.updateOne({ user: user._id },
-                {
-                    $set: {
-                        lastname: req.body.lastname,
-                        firstname: req.body.firstname,
-                        email: req.body.email,
-                        photo: req.body.photo,
-                        games: req.body.games,
-                        socials: {
-                            twitch: req.body.twitch,
-                            instagram: req.body.instagram,
-                            youtube: req.body.youtube,
-                            discord: req.body.discord,
-                        },
-                        about: req.body.about,
-                    },
-                },
-                { new: true })
-                .populate('user')
-                .then(updatedProfile => {
-                    if (!updatedProfile) {
-                        // Profile not found, send a 404 response
-                        res.json({ result: false, message: 'Coach profile not found' });
-                    } else {
-                        // Profile was successfully updated, send the updated profile
-                        res.json({ result: true, profile: updatedProfile });
-                    }
-                })
-            });
-        });
-        
+    // Update the coach profile by user ID
+    CoachProfile.updateOne(
+      { user: user._id },
+      {
+        $set: {
+          lastname: req.body.lastname,
+          firstname: req.body.firstname,
+          email: req.body.email,
+          photo: req.body.photo,
+          games: req.body.games,
+          socials: {
+            twitch: req.body.twitch,
+            instagram: req.body.instagram,
+            youtube: req.body.youtube,
+            discord: req.body.discord,
+          },
+          about: req.body.about,
+          language: req.body.language,
+        },
+      },
+      { new: true }
+    )
+      .populate("user")
+      .then((updatedProfile) => {
+        if (!updatedProfile) {
+          // Profile not found, send a 404 response
+          res.json({ result: false, message: "Coach profile not found" });
+        } else {
+          // Profile was successfully updated, send the updated profile
+          res.json({ result: true, profile: updatedProfile });
+        }
+      });
+  });
+});
+
 // Create a DELETE /profile route to delete the coach informations
-    router.delete('/profile/:coach', (req, res) => {
-        // Search user ID via username
-        UserLogin.deleteOne({username: req.params.coach}) 
-        .then(user => { 
-        // Delete the coach profile by user ID
-        CoachProfile.deleteOne({user:user._id}).populate('user')
-         .then(deletedProfile => {
-            if (!deletedProfile) {
-             // Profile not found, send a 404 response
-            res.json({ result: false, message: 'Coach profile not found' });
-            } else {
-            // Profile was successfully deleted, send a success response
-            res.json({ result: true, message: 'Coach profile deleted successfully' });
-            }
-        })
-    });
-})
+router.delete("/profile/:coach", (req, res) => {
+  // Search user ID via username
+  UserLogin.deleteOne({ username: req.params.coach }).then((user) => {
+    // Delete the coach profile by user ID
+    CoachProfile.deleteOne({ user: user._id })
+      .populate("user")
+      .then((deletedProfile) => {
+        if (!deletedProfile) {
+          // Profile not found, send a 404 response
+          res.json({ result: false, message: "Coach profile not found" });
+        } else {
+          // Profile was successfully deleted, send a success response
+          res.json({
+            result: true,
+            message: "Coach profile deleted successfully",
+          });
+        }
+      });
+  });
+});
 
 //Get all available games from all coaches
-router.get('/games', (req,res) => {
-    CoachProfile.find()
-    .then(coaches =>{
-        let availableGames = []
-        coaches.map((coach) => availableGames=[...new Set(coach.games)])
-        res.json({result:true, availableGames})
-    })
-})
+router.get("/games", (req, res) => {
+  CoachProfile.find().then((coaches) => {
+    let availableGames = [];
+    for (let coach of coaches) {
+      for (let game of coach.games) {
+        if (!availableGames.includes(game)) {
+          availableGames.push(game);
+        }
+      }
+    }
+    res.json({ result: true, availableGames });
+  });
+});
+
+// Get all available languages from all coaches
+router.get("/languages", (req, res) => {
+  CoachProfile.find().then((coaches) => {
+    let availableLanguages = [];
+    for (let coach of coaches) {
+      for (let language of coach.language) {
+        if (!availableLanguages.includes(language)) {
+          availableLanguages.push(language);
+        }
+      }
+    }
+    res.json({ result: true, availableLanguages });
+  });
+});
 
 module.exports = router;
