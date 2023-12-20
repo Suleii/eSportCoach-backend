c3f32d9001ee25ab60866402288c4ff8aad83faf
Your name~Suleiman <Your email[201~suleiman.mulla1504@gmail.com>
2023-12-20 14:19:11 +0100

    booking confirmation mail

--- routes/emails.js
+++ routes/emails.js
@@ -1,66 +1,82 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-  
-let nodemailer = require('nodemailer')
+
+let nodemailer = require("nodemailer");
 const pw = process.env.GMAIL_PASSWORD;
 
+router.post("/contact", (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "experience.lacapsule@gmail.com",
+      pass: pw,
+    },
+  });
 
-router.post('/contact', (req, res) =>{
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'experience.lacapsule@gmail.com',
-            pass: pw
-        }
-    });
+  var mailOptions = {
+    from: "experience.lacapsule@gmail.com",
+    to: "experience.lacapsule@gmail.com",
+    subject: req.body.title,
+    html: `Message from ${req.body.name} (${req.body.email}) : ${req.body.message}`,
+  };
 
-    var mailOptions = {
-        from: 'experience.lacapsule@gmail.com',
-        to: 'experience.lacapsule@gmail.com',
-        subject: req.body.title,
-        html: `Message from ${req.body.name} (${req.body.email}) : ${req.body.message}`,
-        
-    };
-
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            res.json({error});   
-        } else {
-            res.json({sent: info.response});
-            
-        }
-        
-    });
-    
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.json({ error });
+    } else {
+      res.json({ sent: info.response });
+    }
+  });
 });
 
-router.post('/signup', (req, res) =>{
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'experience.lacapsule@gmail.com',
-            pass: pw
-        }
-    });
+router.post("/signup", (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "experience.lacapsule@gmail.com",
+      pass: pw,
+    },
+  });
 
-    var mailOptions = {
-        from: 'experience.lacapsule@gmail.com',
-        to: req.body.email,
-        subject: "Welcome to Experience",
-        html: `Thank you for signing up ${req.body.name}! You can now enjoy our services.`,
-        
-    };
+  var mailOptions = {
+    from: "experience.lacapsule@gmail.com",
+    to: req.body.email,
+    subject: "Welcome to Experience",
+    html: `Thank you for signing up ${req.body.name}! You can now enjoy our services.`,
+  };
 
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            res.json({error});   
-        } else {
-            res.json({sent: info.response});
-            
-        }
-        
-    });
-    
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.json({ error });
+    } else {
+      res.json({ sent: info.response });
+    }
+  });
+});
+
+router.post("/bookingConfirmation", (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "experience.lacapsule@gmail.com",
+      pass: pw,
+    },
+  });
+
+  var mailOptions = {
+    from: "experience.lacapsule@gmail.com",
+    to: req.body.email,
+    subject: "Booking confirmed !",
+    html: ` We are please to announce that your booking with ${req.body.coachName} at ${req.body.date} is confirmed ! You can now prepare your session.`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.json({ error });
+    } else {
+      res.json({ sent: info.response });
+    }
+  });
 });
 
 module.exports = router;
